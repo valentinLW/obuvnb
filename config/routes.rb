5012baf181102ecbc7e123ceda720a00a966fa1f
Ruy Schneider <ruy.schneider@icloud.com>
2021-08-17 15:21:10 +0200

    navbar-first-deploy

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :shoes, only: %i[new create index show edit update]
+  get "about", to: 'pages#about'
 end
