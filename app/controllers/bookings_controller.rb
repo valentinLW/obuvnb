8922da8bfeaf20ebc2592a2fbbf65544ef1a7b2c
Marina Miranda <mirandaxmarina@gmail.com>
2021-08-17 11:37:13 +0200

    bookings/new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @shoe = Shoe.find(params[:shoe_id])
+  end
+
+  def create
+    @booking = Booking.create(booking_params)
+  end
+
+  def show
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:pick_up_date, :return_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :shoes, only: %i[new create index show edit update]
-  resources :bookings, only: %i[new create show]
+  resources :shoes, only: %i[new create index show edit update] do
+    resources :bookings, only: %i[new create show]
+  end
 end
