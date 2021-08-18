f476e987fcd612e85e584b971c7efbc7deb1e85a
Ruy Schneider <ruy.schneider@icloud.com>
2021-08-18 14:54:46 +0200

    3 pictures per shoe

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,7 +63,7 @@ file = URI.open('https://images.stockx.com/360/Nike-Dunk-Low-SE-Multi-Camo/Image
   )
 shoe.image1.attach(io: file, filename: 'image1', content_type: 'image/png')
 
-12.times do
+30.times do
 stockx_photos = %w[
 https://images.stockx.com/360/Air-Jordan-1-Retro-High-Electro-Orange/Images/Air-Jordan-1-Retro-High-Electro-Orange/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1626802469&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=1140
 https://images.stockx.com/360/adidas-Yeezy-Foam-RNNR-MX-Cream-Clay/Images/adidas-Yeezy-Foam-RNNR-MX-Cream-Clay/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1628003512&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=1140
@@ -76,8 +76,31 @@ https://images.stockx.com/360/Nike-SB-Dunk-Low-Ben-Jerrys-Chunky-Dunky/Images/Ni
 https://images.stockx.com/images/Air-Jordan-1-High-Zoom-CMFT-Lemon-Twist-W.jpg?fit=clip&bg=FFFFFF&auto=compress&dpr=2&trim=color&updated_at=1622155206&fm=webp&ixlib=react-9.1.5&q=90&w=1140&h=500
 https://images.stockx.com/images/Air-Jordan-6-Retro-Travis-Scott-British-Khaki-TD-Ver2.jpg?fit=clip&bg=FFFFFF&auto=compress&dpr=2&trim=color&updated_at=1621999290&fm=webp&ixlib=react-9.1.5&q=90&w=1140&h=500
 https://images.stockx.com/images/adidas-Harden-Vol-5-Solar-Yellow.jpg?fit=clip&bg=FFFFFF&auto=compress&dpr=2&trim=color&updated_at=1616168708&fm=webp&ixlib=react-9.1.5&q=90&w=1140&h=500
+https://images.stockx.com/images/adidas-Yeezy-450-Dark-Slate-Product.jpg?fit=clip&bg=FFFFFF&auto=compress&dpr=2&trim=color&updated_at=1626801847&fm=webp&ixlib=react-9.1.5&q=90&w=400&h=500
+https://images.stockx.com/images/Air-Jordan-14-Retro-Aleali-May-W-Ver2.jpg?fit=clip&bg=FFFFFF&auto=compress&dpr=2&trim=color&updated_at=1628639453&fm=webp&ixlib=react-9.1.5&q=90&w=400&h=500
+https://images.stockx.com/360/adidas-Yeezy-Boost-700-MNVN-Blue-Tint/Images/adidas-Yeezy-Boost-700-MNVN-Blue-Tint/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1626289128&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/images/nike-sb-dunk-low-barcelona.jpg?fit=clip&bg=FFFFFF&auto=compress&dpr=2&trim=color&updated_at=1626158008&fm=webp&ixlib=react-9.1.5&q=90&w=400&h=500
+https://images.stockx.com/images/Nike-Dunk-SB-Low-Shrimp.jpg?fit=clip&bg=FFFFFF&auto=compress&dpr=2&trim=color&updated_at=1608510815&fm=webp&ixlib=react-9.1.5&q=90&w=400&h=500
+https://images.stockx.com/360/Nike-Dunk-SB-Low-Concepts-Grail/Images/Nike-Dunk-SB-Low-Concepts-Grail/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1607647784&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/360/Nike-SB-Dunk-Low-Broncos/Images/Nike-SB-Dunk-Low-Broncos/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1624633731&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/360/Nike-Dunk-SB-Low-Nontourage/Images/Nike-Dunk-SB-Low-Nontourage/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1607052832&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/images/Nike-SB-Dunk-Low-Strummer.png?fit=clip&bg=FFFFFF&auto=compress&dpr=2&trim=color&updated_at=1625852297&fm=webp&ixlib=react-9.1.5&q=90&w=400&h=500
+https://images.stockx.com/360/Nike-SB-Dunk-Low-Bic/Images/Nike-SB-Dunk-Low-Bic/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1608512122&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/360/Nike-Dunk-SB-Low-Oompa-Loompa/Images/Nike-Dunk-SB-Low-Oompa-Loompa/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1607053319&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/360/Nike-Dunk-SB-High-Huxtable/Images/Nike-Dunk-SB-High-Huxtable/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1607047838&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/360/Nike-SB-Dunk-High-Bota/Images/Nike-SB-Dunk-High-Bota/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1607060249&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/360/Nike-Dunk-SB-High-Quagmire/Images/Nike-Dunk-SB-High-Quagmire/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1607054461&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/360/Nike-SB-Dunk-High-Humidity/Images/Nike-SB-Dunk-High-Humidity/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1607058513&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/360/Nike-Dunk-SB-High-HUF/Images/Nike-Dunk-SB-High-HUF/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1608514131&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/360/Nike-SB-Dunk-High-Marge-Simpson/Images/Nike-SB-Dunk-High-Marge-Simpson/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1624633452&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
+https://images.stockx.com/360/Nike-Dunk-SB-High-Concepts-Stained-Glass/Images/Nike-Dunk-SB-High-Concepts-Stained-Glass/Lv2/img01.jpg?auto=compress&dpr=2&updated_at=1607055048&fit=clip&fm=webp&ixlib=react-9.1.5&q=90&w=400
 ]
-file = URI.open(stockx_photos.sample)
+
+shoe_photos = [stockx_photos.sample, stockx_photos.sample, stockx_photos.sample]
+
+file = URI.open(shoe_photos[0])
+file2 = URI.open(shoe_photos[1])
+file3 = URI.open(shoe_photos[2])
   shoe = Shoe.create(
     model: "Nike #{Faker::JapaneseMedia::DragonBall.character}",
     price: rand(23..999),
@@ -87,4 +110,6 @@ file = URI.open(stockx_photos.sample)
     user_id: User.all.sample.id
   )
 shoe.image1.attach(io: file, filename: 'image1', content_type: 'image/png')
+shoe.image2.attach(io: file2, filename: 'image2', content_type: 'image/png')
+shoe.image3.attach(io: file3, filename: 'image3', content_type: 'image/png')
 end
