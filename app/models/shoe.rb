baa71486b2c6a491e9143219421e4ca740734bd2
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-08-19 12:02:37 +0200

    basic upload multiple shoes

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,8 +1,4 @@
 class Shoe < ApplicationRecord
   belongs_to :user
-  has_one_attached :image1
-  has_one_attached :image2
-  has_one_attached :image3
-  has_one_attached :image4
-  has_one_attached :image5
+  has_many_attached :images
 end
