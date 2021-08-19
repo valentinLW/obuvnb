116f3cffa651056bd38c02d7f17124ca505b5e82
Ruy Schneider <ruy.schneider@icloud.com>
2021-08-19 12:52:43 +0200

    database updated. booking status default pending

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :shoe
   belongs_to :user
+  enum status: %i[pending accepted rejected]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_091409) do
+ActiveRecord::Schema.define(version: 2021_08_19_102730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_08_17_091409) do
     t.integer "total_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0
     t.index ["shoe_id"], name: "index_bookings_on_shoe_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
