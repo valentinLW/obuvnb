f1bbd88b6bda68b57ddce362cba09c67dccc8eb8
Ruy Schneider <ruy.schneider@icloud.com>
2021-08-19 15:34:12 +0200

    datepickrinprogress

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -5,4 +5,10 @@ class Shoe < ApplicationRecord
   has_one_attached :image3
   has_one_attached :image4
   has_one_attached :image5
+
+  def unavailable_dates
+    booking.pluck(:pick_up_date, :return_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
